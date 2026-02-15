a8b72e4c0d3ccfb833bfb33f551ddafaa659cfed
hzkfs0121 <you@example.com>
2026-02-15 18:02:36 +0300

    migration

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -e
 # Install Python dependencies
 pip install -r requirements.txt
 
+# Run database migrations
+python manage.py migrate --noinput
+
 # Collect static files
 python manage.py collectstatic --noinput
